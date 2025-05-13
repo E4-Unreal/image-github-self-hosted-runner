b42dd2c37204db10c943e4eeea134579a4ac0fa7
이정훈 <eu4ng97@gmail.com>
2025-05-13 18:22:20 +0900

    Feat: GitHub Self-hosted Runner 설치를 위한 도커 이미지 관련 파일 추가

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,28 @@
-FROM alpine:latest
+FROM ubuntu:latest
 
 LABEL maintainer="eu4ng97@gmail.com"
 LABEL version="0.1.0"
 LABEL description=""
 
-RUN apk update --no-cache  \
-    && apk add --no-cache tzdata
+# 패키치 설치 및 업데이트
+RUN apt-get update  \
+    && apt-get install -y tzdata curl libicu-dev jq \
+    && apt-get clean
 
-ENV TZ=Asia/Seoul
+ENV TZ=Asia/Seoul  \
+    RUNNER_ALLOW_RUNASROOT=1  \
+    GITHUB_ORGANIZATION=ORG
 
+# 작업 디렉토리 설정
+WORKDIR /actions-runner
+
+# init.sh 파일 추가
 COPY init.sh init.sh
 RUN chmod +x init.sh
+
+# runner 설치
+RUN curl -o actions-runner-linux-x64-2.323.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.323.0/actions-runner-linux-x64-2.323.0.tar.gz  \
+    && tar xzf ./actions-runner-linux-x64-2.323.0.tar.gz  \
+    && rm -rf actions-runner-linux-x64-2.323.0.tar.gz
 
 ENTRYPOINT [ "./init.sh" ]
--- init.sh
+++ init.sh
@@ -1,4 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "$TZ"
-date
+register_actions_runner()
+{
+    actions_runner_token="$(cat /run/secrets/actions_runner_token)"
+    response=$(curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $actions_runner_token" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/orgs/$GITHUB_ORGANIZATION/actions/runners/registration-token")
+    token=$(echo "$response" | jq -r '.token')
+    ./config.sh --url https://github.com/"$GITHUB_ORGANIZATION" --token "$token"
+}
+
+unregister_actions_runner()
+{
+    actions_runner_token="$(cat /run/secrets/actions_runner_token)"
+    response=$(curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $actions_runner_token" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/orgs/$GITHUB_ORGANIZATION/actions/runners/remove-token")
+    token=$(echo "$response" | jq -r '.token')
+    ./config.sh remove --token "$token"
+}
+
+register_actions_runner
+trap unregister_actions_runner EXIT
+./run.sh
